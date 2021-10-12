87a5bac16a44c65a2b0bab867c6f99266cd35632
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-12 14:04:17 -0500

    created db inserts in database.sql file

--- database.sql
+++ database.sql
@@ -3,8 +3,28 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+--create a database named "their_two_cents"
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "category" (
+	"id" SERIAL PRIMARY KEY,
+	"category_name" VARCHAR (1000) NOT NULL,
+	"user_id" INT REFERENCES "user" NOT NULL
+
+);
+
+CREATE TABLE "recommendation" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (1000) NOT NULL,
+    "type" VARCHAR (1000) NOT NULL,
+    "notes" VARCHAR (1500) NOT NULL,
+    "favorite" BOOLEAN NOT NULL DEFAULT FALSE,
+    "user_id" INT REFERENCES "user" NOT NULL
+
 );
