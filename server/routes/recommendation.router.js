7c34f7d4101dfc2a561b9dab3f223f65a8006ea5
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-13 14:14:03 -0500

    first attempt at post route on server side

--- server/routes/recommendation.router.js
+++ server/routes/recommendation.router.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   const queryText = `SELECT * FROM "recommendation" ORDER BY "name"`;
   pool.query(queryText)
     .then(result => {
-      console.log('the result is', result );
+      console.log('the result is', result);
       res.send(result.rows);
     }).catch(error => {
       console.log('Error getting recommendations from DB!', error);
@@ -15,11 +15,29 @@ router.get('/', (req, res) => {
     })
 });
 
-/**
- * POST route template
- */
+//POST route to send new user recommendation to the db from add a recommendation page
 router.post('/', (req, res) => {
-  // POST route code here
+  const newRecommendation = req.body;
+  console.log('The new rec is', newRecommendation);
+  const insertRecQuery = `INSERT INTO "recommendation" ("name", "type", "notes") VALUES ($1, $2, $3) RETURNING "id"`;
+  pool.query(insertRecQuery, [newRecommendation.name, newRecommendation.type, newRecommendation.notes])
+    .then(result => {
+      console.log('New Recommendation id is', result.rows[0].id);
+    }).catch(error => {
+      console.log('error with first post to db', error);
+      res.sendStatus(500);
+    })
+  const createdRecId = result.rows[0].id;
+  const insertCategoryQuery = `INSERT INTO "category" ("category_name") VALUES ($1)`;
+  pool.query(insertCategoryQuery, [createdRecId, Number(req.body.category)])
+    .then(result => {
+      res.sendStatus(201);
+    }).catch(error => {
+      console.log('Error with sending recommendation to db', error);
+      res.sendStatus(500);
+    })
+
+
 });
 
 module.exports = router;
