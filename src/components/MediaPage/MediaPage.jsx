4a2056cace2fa458bcd7158aeb2341a9844e7f62
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-15 12:56:16 -0500

    working on PUT/favorite

--- src/components/MediaPage/MediaPage.jsx
+++ src/components/MediaPage/MediaPage.jsx
@@ -1,25 +1,51 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
-function MediaPage () {
+function MediaPage() {
 
     const dispatch = useDispatch();
-    const media = useSelector(store => store.recommendations.media );
+    const media = useSelector(store => store.recommendations.media);
 
-    useEffect(()=> {
-        dispatch({type: 'FETCH_MEDIA'});
+    useEffect(() => {
+        dispatch({ type: 'FETCH_MEDIA' });
     }, []);
 
-    return(
+    const deleteRecommendation = () => {
+        console.log('in deleteFunction');
+        dispatch({})
+    }
+
+    return (
 
         <>
-        {media.map((media) =>
-         <div key={media.id}>{media.name}
-         {media.type}
-         {media.notes}
-          </div>
-      
-      )}
+            {media.map((media) =>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Notes</th>
+                            <th>Favorite</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{media.name}</td>
+                            <td>{media.type}</td>
+                            <td>{media.notes}</td>
+                            <td><button>Favorite!</button></td>
+                            <td><button onClick={deleteRecommendation}>Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+            )}
+
+
+
         </>
     )
 }
