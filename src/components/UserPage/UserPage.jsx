1ddb8a8c2941e1248a295909dfc86544c9242e6c
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-17 16:00:51 -0500

    built table on home page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,36 +1,71 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector(store => store.user);
   const dispatch = useDispatch();
+  const history = useHistory();
   const recommendations = useSelector(store => store.recommendations.recommendations);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_RECOMMENDATIONS' });
   }, []);
 
-  console.log('The recommendations are', recommendations );
+  console.log('The recommendations are', recommendations);
 
 
   return (
-  
+
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-    
+
       {/* {JSON.stringify({ recommendations })}  */}
-       {recommendations.map((recommendation) =>
-         <div key={recommendation.id}>{recommendation.name}
-         {recommendation.type}
-         {recommendation.notes}
-          </div>
-      
+      {recommendations.map((recommendation) =>
+        <div key={recommendation.id}>{recommendation.name}
+          {recommendation.type}
+          {recommendation.notes}
+        </div>
+
       )}
 
+      <table>
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Count</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Media</td>
+            <td>number of recs</td>
+            <td><button>Details</button></td>
+          </tr>
+          <tr>
+            <td>Products</td>
+            <td>number of recs</td>
+            <td><button>Details</button></td>
+          </tr>
+          <tr>
+            <td>Businesses</td>
+            <td>number of recs</td>
+            <td><button>Details</button></td>
+          </tr>
+          <tr>
+            <td>Other</td>
+            <td>number of recs</td>
+            <td><button>Details</button></td>
+          </tr>
+        </tbody>
+
+      </table>
+
     </div>
- 
+
 
   );
 }
