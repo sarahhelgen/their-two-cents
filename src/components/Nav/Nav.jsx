af7734a238aec619d8ea6e21cc84d8a47fd0708f
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-19 03:45:56 -0500

    basic drawer complete!

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,16 +10,27 @@ import Drawer from '@mui/material/Drawer';
 import ListItem from '@mui/material/ListItem';
 import Typography from '@mui/material/Typography';
 import { makeStyles } from '@mui/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddIcon from '@mui/icons-material/Add';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import HomeIcon from '@mui/icons-material/Home';
+import ListItemText from '@mui/material/ListItemText';
+import TheatersIcon from '@mui/icons-material/Theaters';
+import BusinessIcon from '@mui/icons-material/Business';
+import NordicWalkingIcon from '@mui/icons-material/NordicWalking';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import TerrainIcon from '@mui/icons-material/Terrain';
+import WidgetsIcon from '@mui/icons-material/Widgets';
 
 
 
 function Nav() {
 
   const user = useSelector((store) => store.user);
-  const [state, setState] = useState(false);
+  const [drawerState, setDrawerState] = useState(false);
 
   const toggleDrawer = (open) => (event) => {
-    setState(open)
+    setDrawerState(open)
   }
 
   const drawerWidth = 240;
@@ -46,16 +57,73 @@ function Nav() {
 
   const list = () => (
     <div onClick={toggleDrawer(false)}>
-      <Typography variant="h5">
+      <Typography variant="h6">
+        
         <List>
-          <ListItem>Add a Recommendation</ListItem>
-          <ListItem>Media</ListItem>
-          <ListItem>Products</ListItem>
-          <ListItem>Businesses</ListItem>
-          <ListItem>Other</ListItem>
-          <ListItem>Favorites!</ListItem>
-          <ListItem>Home</ListItem>
-          <ListItem>Logout</ListItem>
+
+          <Link to="/user">
+          <ListItem button>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Home"}/>
+          </ListItem>
+          </Link>
+
+          <Link to="/add">
+          <ListItem button>
+                <ListItemIcon>
+                  <AddIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Add Recommendation"} />
+            </ListItem>
+            </Link>
+        
+        <Link to="/media">
+        <ListItem button>
+          <ListItemIcon>
+            <TheatersIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Media"} />
+        </ListItem>
+        </Link>
+
+        <Link to="/business">
+          <ListItem button>
+            <ListItemIcon>
+                <BusinessIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Business"} />
+          </ListItem>
+        </Link>
+        
+        <Link to="/product">
+          <ListItem button>
+            <ListItemIcon>
+                <WidgetsIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Product"} />
+          </ListItem>
+        </Link>
+
+        <Link to="/other">
+          <ListItem button>
+            <ListItemIcon>
+              <TerrainIcon />
+            </ListItemIcon>
+              <ListItemText primary={"Other"} />
+          </ListItem>
+        </Link>
+
+        <Link to="/favorite">
+          <ListItem button>
+            <ListItemIcon>
+              <FavoriteIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Favorites"} />
+          </ListItem>
+        </Link>
+
         </List>
       </Typography>
     </div>
@@ -78,6 +146,7 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
+          <MenuIcon />
             <Link className="navLink" to="/user">
               Home
             </Link>
@@ -116,7 +185,7 @@ function Nav() {
             variant="temporary"
             anchor={'left'}
             classes={{ paper: classes.drawerPaper }}
-            open={state}
+            open={drawerState}
             onClose={toggleDrawer(false)}
           >
             {list()}
