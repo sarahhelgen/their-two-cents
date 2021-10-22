a989ca7869016f2614a452a0c6465a59b353e59b
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-21 19:35:30 -0500

    updated env file

--- server/routes/twilio.router.js
+++ server/routes/twilio.router.js
@@ -2,18 +2,30 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+    .create({
+        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+        from: '+15017122661',
+        to: '+15558675310'
+    })
+    .then(message => console.log(message.sid));
+
 /**
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+    // GET route code here
 });
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
-  // POST route code here
+    // POST route code here
 });
 
 module.exports = router;
